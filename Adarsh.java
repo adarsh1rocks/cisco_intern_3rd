a2a504c5dd3a8986a69cfa68c7902be52b06c37b
adarsh anand <adarsh.anand2015@vit.ac.in>
2019-01-29 14:21:19 +0530

    first commit

---
+++ Adarsh.java
@@ -0,0 +1,7 @@
+class Adarsh
+{
+	public static void main(String [] args)
+	{
+		System.out.println("hello welocme to github and we will start fresh and continue on this untill we get confortable");
+	}
+}
